e173a81c05fce629c50fa617fe047011def56abd
Atul Sehgal <atulshgl@gmail.com>
2017-04-09 16:10:36 -0700

    Update parser_driver.py

--- parser_driver.py
+++ parser_driver.py
@@ -4,15 +4,16 @@ import os, codecs, time
 start_time = time.time()
 
 cur_dir = os.path.join(os.path.dirname(__file__))
-input_file = cur_dir + '/article.txt'
+input_file = cur_dir + 'article.txt'
+output_file = cur_dir + 'phrases.txt'
 
 # Add the jar and model via their path (instead of setting environment variables):
 # Path to spanish POS tagger
-jar = cur_dir + '/stanford-postagger-full-2016-10-31/stanford-postagger.jar'
-model = cur_dir + '/stanford-postagger-full-2016-10-31/models/spanish-distsim.tagger'
+jar = cur_dir + 'stanford-postagger-full-2016-10-31/stanford-postagger.jar'
+model = cur_dir + 'stanford-postagger-full-2016-10-31/models/spanish-distsim.tagger'
 
 # Path to spanish parser
-stanford_parser_dir = cur_dir + '/stanford-parser-full-2016-10-31/'
+stanford_parser_dir = cur_dir + 'stanford-parser-full-2016-10-31/'
 esp_model_path = stanford_parser_dir  + "stanford-parser-3.7.0-models/edu/stanford/nlp/models/lexparser/spanishPCFG.ser.gz"
 my_path_to_models_jar  = stanford_parser_dir + "stanford-parser-3.7.0-models.jar"
 my_path_to_jar = stanford_parser_dir  + "stanford-parser.jar"
@@ -43,6 +44,12 @@ def readInput(file_path):
             sentences += para.split('.')
     return filter(None, map(unicode.strip, sentences))
 
+def savePhrases(output_file, phrases):
+    with codecs.open(output_file,'w', 'utf-8') as f:
+        for phrase in phrases:
+            f.write(phrase + u'\n')
+
+
 sentences = readInput(input_file)
 
 parser = spanishParser(esp_model_path, my_path_to_models_jar, my_path_to_jar)
@@ -53,7 +60,6 @@ for line in sentences:
     parser.parse(line)
     phrases += parser.getPhrase('grup.nom')
 
-for phrase in set(phrases):
-    print phrase
+savePhrases(output_file, set(phrases))
 
 print("--- %s seconds ---" % (time.time() - start_time))
